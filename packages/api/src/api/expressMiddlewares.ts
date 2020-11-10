6ebb01707ae4a64a287eb95b260cbcc883207974
Hariom Sharma <hariom@appointy.com>
2020-11-10 19:44:49 +0530

    fix: updated env

--- packages/api/src/api/expressMiddlewares.ts
+++ packages/api/src/api/expressMiddlewares.ts
@@ -3,16 +3,22 @@ import express from 'express'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 
+const isDev = process.env.NODE_ENV === 'development'
+
 export const getExpressMiddlewares = () => {
     const staticMiddleware = express.static('../web/build')
-    const corsMiddleware = cors({
-        origin: [process.env.APP_URL!],
-        credentials: true
-    })
+    const corsMiddleware = isDev
+        ? cors({
+              origin: [process.env.APP_URL!],
+              credentials: true
+          })
+        : undefined
     const cookieMiddleware = cookieParser()
     const bodyJsonMiddleware = bodyParser.json()
     const bodyUrlEncodeMiddleware = bodyParser.urlencoded({
         extended: true
     })
-    return [staticMiddleware, corsMiddleware, cookieMiddleware, bodyJsonMiddleware, bodyUrlEncodeMiddleware]
+    return [staticMiddleware, corsMiddleware, cookieMiddleware, bodyJsonMiddleware, bodyUrlEncodeMiddleware].filter(
+        Boolean
+    )
 }
